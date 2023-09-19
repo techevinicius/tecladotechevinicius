a3d98ab6e609e9568e793a7521189b2bb1fe1d66
techevinicius <108283361+techevinicius@users.noreply.github.com>
2023-09-19 11:26:46 -0300

    main

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function tocarSompuff (){
 }
 
 function tocarSomsplash (){
-    document.querySelector('#som_tecla_slapsh').play();
+    document.querySelector('#som_tecla_splash').play();
     document.querySelector('.tecla_splash').onclick = tocarSomsplash;
 }
 
